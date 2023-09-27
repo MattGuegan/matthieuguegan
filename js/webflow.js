cac233df15b34c51d8961eeb16a1347396183293
MattGuegan <141003676+MattGuegan@users.noreply.github.com>
2023-09-27 06:48:00 +0200

    Update webflow.js

--- js/webflow.js
+++ js/webflow.js
@@ -758,38 +758,6 @@
     }
   });
 
-  $(document).ready(function() {
-  $('[svg="animated"]').css({
-    opacity: 0,
-    transition: "opacity 400ms ease"
-  });
-
-  $(window).on("load resize scroll", function() {
-    $('[svg="animated"] path').each(function() {
-      var pathLength = this.getTotalLength();
-      $(this).attr({
-        "stroke-dasharray": pathLength,
-        "stroke-dashoffset": pathLength
-      });
-      var svgAnimated = $(this).closest('[svg="animated"]');
-      var svgAnimatedTop = svgAnimated.offset().top;
-      var svgAnimatedHeight = svgAnimated.outerHeight();
-      var windowHeight = $(window).height();
-      var windowScrollTop = $(window).scrollTop();
-      var animationDuration = svgAnimated.attr('svg-animation-time') || 5000;
-
-      if (windowScrollTop + windowHeight > svgAnimatedTop && windowScrollTop < svgAnimatedTop + svgAnimatedHeight) {
-        $(svgAnimated).css("opacity", 1);
-        $(this).css({
-          transition: "stroke-dashoffset " + animationDuration + "ms ease-out",
-          "stroke-dashoffset": 0
-        });
-      }
-    });
-  });
-});
-
-
   // packages/shared/render/plugins/BaseSiteModules/underscore-custom.js
   var require_underscore_custom = __commonJS({
     "packages/shared/render/plugins/BaseSiteModules/underscore-custom.js"(exports2, module2) {
